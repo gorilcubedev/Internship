e5f3ebb2ef4de11c0a10eb6f02e1abdf60d9d4a9
Emir Berk Öncü <emirberkoncu@gmail.com>
2025-07-29 18:01:28 +0300

    4. Ders

--- index.py
+++ index.py
@@ -1,7 +1,39 @@
 
-print('Welcome to Python 101!')
-print('Welcome to Python 1012!')
-print('Create Hammer')
-print('Create Nails')
-print('Use Hammer')
-print('Use Hammer and Nails')
+# 1
+# print('Welcome to Python 101!')
+# print('Welcome to Python 1012!')
+# print('Create Hammer')
+# print('Create Nails')
+# print('Use Hammer')
+# print('Use Hammer and Nails')
+
+
+
+# 2 -> Concatenation
+# failed_subjects = "6"
+# name = 'John'
+
+# print('Dear Mrs Badger')
+# print('Your son' + name + ' is failing ' + failed_subjects + ' subjects.')
+# print(name  + ' will need to redo ' + failed_subjects +  ' courses.')
+# name = 'Eric'
+# print(name + ' is doing well in geography.')
+
+
+# 3 -> Variables
+# failed_subjects = 2.45 # Float
+# name = 'John' # String
+# is_going_to_party = True # Boolean
+# a = "it's"
+# b = 'it\'s'
+# print('Dear Mrs Badger')
+# print('Your son' + name + ' is failing ' + failed_subjects + ' subjects.')
+# print(name  + ' will need to redo ' + failed_subjects +  ' courses.')
+# name = 'Eric'
+# print(name + ' is doing well in geography.')
+
+# 4 -> Type
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
