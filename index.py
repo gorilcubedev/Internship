13c5dc097e595527e22066cfbb438cb01a0565be
Emir Berk Öncü <emirberkoncu@gmail.com>
2025-07-29 19:13:19 +0300

    Pit Stop  - Exercise

--- index.py
+++ index.py
@@ -116,7 +116,46 @@
 # print(msg[-4:])
 
 # Exercise -> Slicing 
-msg='welcome to Python 101: Strings'
-msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
-print(msg1.title())
-print(msg1[::-1].title())
+# msg='welcome to Python 101: Strings'
+# msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
+# print(msg1.title())
+# print(msg1[::-1].title())
+
+
+# Find / Replace / String Formatting
+# msg = 'Welcome to Python 101: Strings'
+# print('Python' not in msg)
+
+# name='TERRY'
+# color = 'RED'
+# msg = '[' + name + '] loves the color ' + color + '!'
+# msg1 = f'[{name}] loves the color {color.lower()}!'
+# print(msg)
+
+
+def pit_stop_optimizer():
+    print("🏁 Welcome to the Pit Stop Timing Optimizer! 🔧\n")
+    
+    # Get race information from user
+    total_race_time = float(input("Enter the total race time in seconds: "))
+    num_pit_stops = int(input("How many pit stops were made? "))
+    avg_pit_duration = float(input("Enter the average pit stop duration in seconds: "))
+    
+    # Calculate total pit stop time
+    total_pit_time = num_pit_stops * avg_pit_duration
+    
+    # Calculate percentage of race spent in pits
+    pit_percentage = (total_pit_time / total_race_time) * 100
+    pit_percentage = round(pit_percentage, 2)
+    
+    # Display results
+    print(f"\n📊 Pit Stop Analysis Results:")
+    print(f"Total pit stop time: {total_pit_time} seconds")
+    print(f"Percentage of race time spent in pits: {pit_percentage}%")
+    
+    # Check if pit time is too high
+    if pit_percentage > 5:
+        print("You need a new pit crew. 🛠️")
+
+if __name__ == "__main__":
+    pit_stop_optimizer()
